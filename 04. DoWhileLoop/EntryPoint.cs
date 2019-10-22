cc587ea99964ab237ffd1b9616769b981408265e
Lockeheede <prescott.lawani@gmail.com>
2019-10-22 18:46:27 -0500

    Added DoWhile and ForLoops

--- 04. DoWhileLoop/EntryPoint.cs
+++ 04. DoWhileLoop/EntryPoint.cs
@@ -1,8 +1,20 @@
 ﻿using System;
-class Program
+class EntryPoint
 {
+    //A do-while loop is exactly the same as a while loop with a slight difference
+
     static void Main()
     {
+        int firstNumber = 4;
+        int endNumber = 10;
+
+        do
+        {
+            Console.WriteLine(firstNumber);
+            firstNumber++;
+        } while (firstNumber <= endNumber);
+        //The difference is you do the loop at least once and then you check if the information is correct and continue
+        //So, even if the firstNumber was bigger than the endNumber, it would still work at least once.
     }
 }
 
---
+++ 05. ForLoops/EntryPoint.cs
@@ -0,0 +1,18 @@
+﻿using System;
+class EntryPoint
+{
+    //The for loop is similar to the while loop, but with less writing. The for loop always increases on an int value
+    //While the while is based on booleans
+    static void Main()
+    {
+        int number = 10;
+
+        for(int i = 0; i < number; i++)//The i is the indexing variable that is used to operate against another value
+            //It can be a variable (number) or another number
+        {
+            Console.WriteLine(i);
+        }
+
+    }
+}
+
