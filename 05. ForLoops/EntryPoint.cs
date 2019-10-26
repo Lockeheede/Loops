e1799522ed4bc0cffdd019fbf22fd2b26ebb17fa
Lockeheede <prescott.lawani@gmail.com>
2019-10-26 10:11:10 -0500

    Added more for loop practice, as well as automatic method refractoring, the use of array methods, and more switch commands.

--- 05. ForLoops/EntryPoint.cs
+++ 05. ForLoops/EntryPoint.cs
@@ -5,13 +5,58 @@ class EntryPoint
     //While the while is based on booleans
     static void Main()
     {
+        Random rand = new Random();
         int number = 10;
 
-        for(int i = 0; i < number; i++)//The i is the indexing variable that is used to operate against another value
+        for(int i = 1; i <= number; i++)//The i is the indexing variable that is used to operate against another value
             //It can be a variable (number) or another number
         {
             Console.WriteLine(i);
         }
+        //Challenge: Create an integer array and output all of its elements using a for loop
+        int[] numbers = {0,1,4,5,7,8,9,12,1111 };
+
+        for(int i = 0; i < numbers.Length; i++)
+        {
+            //You can multiple the number in the index by the index: numbers[i] *= i; 
+            
+            Console.WriteLine($"Current index is {i}, current element is {numbers[i]}");
+            
+        }
+        //Solution: When using arrays in for loops, first you need actual values. You increase along
+        //The indexing variable, putting it's increase as a location holder for the array
+        //ie: since i starts at 0, you are starting at the 1st index
+        //If it was something else, like i=3, we would get the 4th index of the array
+
+        //You use the .Length operator on numbers in case the array changes
+
+        int[] randNumbers = new int[10];
+        for (int i = 0; i < randNumbers.Length; i++)
+        {
+            //You can generate random numbers for an undeclared set of numbers in an array
+            randNumbers[i] = rand.Next(1,777);
+            Console.WriteLine($"The current index is {i}, it's random number is {randNumbers[i]}");
+        }
+
+        int[] byNumNumbers = { 5, 6, 7, 8, 33, 36, 33, 39, 44, 1111, 666, 777, 86, 83, 24, 21, 4, 69, 63, 64, 52, 51, 55, 9 };
+        for (int i = 0; i < byNumNumbers.Length; i++)
+        {
+            //You can post each number that is an even index number. Remember if a number divided by a modulus equals 0 and it
+            //is two, it is even, or one, it is odd.
+            if(i % 2 == 0)
+            {
+                Console.WriteLine($"The current index is {i}, an even number. It's element is {byNumNumbers[i]}");
+            }
+        }
+        for(int i = 0; i < byNumNumbers.Length; i++)
+        {
+            if(i % 3 == 0)
+            {
+                //Or each index that is by three. 
+                Console.WriteLine($"The current index is {i}, a number by three. It's element is {byNumNumbers[i]}");
+            }
+        }
+        
 
     }
 }
---
+++ 06. ForLoopsPractice/EntryPoint.cs
@@ -0,0 +1,158 @@
+﻿using System;
+class EntryPoint
+{
+    static void Main()
+    {
+        int choice = 0;
+        bool newChoice = false;
+        string[] characters = new string[100];
+        int chosenCharacter = 0;//This was in case 2 but was changed into a global variable so it can be used across all cases
+        //Remember, global variables are in the main function outside of loops. Local variables inside of loops and other 
+        //functions can only be used within said loops and variables
+
+        Console.Write("This is a character collection system." +
+                "\nYou can add up to 100 characters. " +
+                "\nFollow the directions.\n");
+
+        while (!newChoice)
+        {
+                Console.Write("1. Add Character" +
+                "\n2. Edit Character" +
+                "\n3. Remove Character" +
+                "\n4. View Character List" +
+                "\n5. Exit" +
+                "\nYour Choice: ");
+
+            choice = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
+
+            switch(choice)
+            {
+
+                case 1://Adding a character
+                    //Use refracting > extract method or ctrl + r + m to make an automatic custom method
+                    //Just highlight the entire method and do the quick key
+                    AddItem(characters);
+                    Console.WriteLine();
+                    break;
+
+                case 2://Editing an added character
+
+                    chosenCharacter = EditItem(characters);
+                    break;
+
+
+                case 3://Removing an added character
+                    chosenCharacter = RemoveItem(characters);
+                    break;
+
+                case 4://Viewing character list
+                    ViewItem(characters);
+
+                    break;
+
+                case 5:
+                    newChoice = true;
+                    break;
+
+                default:
+                    Console.WriteLine("Invalid choice, please choose between 1 and 4!");
+                    break;
+            }
+        }
+    }
+    //Automatically created methods end up being created underneath the main method
+
+    private static void ViewItem(string[] characters)
+    {
+        Console.WriteLine("Current characters :");
+        for (int i = 0; i < characters.Length; i++)//If you type for and press tab twice it makes an automatic for loop method
+        {
+            if (characters[i] != null)
+            {
+                Console.WriteLine($"{i + 1} : {characters[i]}");
+            }
+        }
+        Console.WriteLine();
+    }
+
+    private static int RemoveItem(string[] characters)
+    {
+        int chosenCharacter;
+        Console.Write("Which character do you want to remove? Between (1-100): ");
+        chosenCharacter = Convert.ToInt32(Console.ReadLine());
+        if (characters[chosenCharacter - 1] != null)
+        {
+            Console.Write($"The character you chose is ");
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(characters[chosenCharacter - 1]);
+            Console.ResetColor();
+            characters[chosenCharacter - 1] = null;
+            Console.WriteLine("It is now empty!");
+
+            //Create a tempArray that will hold the new null character array index
+            //If you don't, when you add a new element, if the null one was before
+            //A filled one, a new added element will be the ahead of the filled one
+            string[] tempArray = new string[characters.Length];
+            int tempArrayIndex = 0;
+
+            for (int i = 0; i < characters.Length; i++)
+            {
+                if (characters[i] != null)
+                {
+                    //In this case, the loop tests to see if the character is not null
+                    tempArray[tempArrayIndex] = characters[i];
+                    //If it is it skips this loop, but if it isn't, the tempArrayIndex
+                    //increases by 1, bringing the character up one in the list
+                    //Until it is no longer a null value
+                    tempArrayIndex++;
+                }
+            }
+            //Use the array class and copy method to move an array into another array
+            Array.Copy(tempArray, characters, tempArray.Length);
+        }
+        else
+        {
+            Console.WriteLine("That is an empty character. Try again");
+        }
+        Console.WriteLine();
+        return chosenCharacter;
+    }
+
+    private static int EditItem(string[] characters)
+    {
+        int chosenCharacter;
+        Console.Write("Which character do you want to edit? Between (1-100): ");
+        chosenCharacter = Convert.ToInt32(Console.ReadLine());
+        if (characters[chosenCharacter - 1] != null)
+        {
+            Console.Write($"The character you chose is ");
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(characters[chosenCharacter - 1]);
+            Console.ResetColor();
+            Console.WriteLine("What would you like to change it to?");
+            characters[chosenCharacter - 1] = Console.ReadLine();
+        }
+        else
+        {
+            Console.WriteLine("That is an empty character. Try again");
+        }
+        Console.WriteLine();
+        return chosenCharacter;
+    }
+
+    private static void AddItem(string[] characters)
+    {
+        for (int i = 0; i < characters.Length; i++)
+        {
+            if (characters[i] == null)
+            {
+                Console.WriteLine("Please add a new character.");
+                characters[i] = Console.ReadLine();
+                break;
+            }
+
+        }
+    }
+}
+
